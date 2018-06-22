b68f3a9d58dda327242b9958d9b2080b428e7836
Philippe Gadiou <philippe.gadiou@gmail.com>
2018-06-22 15:05:49 +0200

    next step each with index and remote buttons

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,4 +8,27 @@ class CompaniesController < ApplicationController
     redirect_to :back, notice: "Companies imported"
   end
 
+  def update
+    @company = Company.find(params[:id])
+    @project_version = @company.project_version
+    @company.update(selection_params)
+    if @product.save
+      respond_to do |format|
+        format.html {redirect_to project_version_path(@project_version)}
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render 'project_versions/show'}
+        format.js
+    end
+  end
+
+  private
+
+  def selection_params
+    params.require(:company).permit(:unrelated_activity, :lack_information, :unrelated_function, :group)
+  end
+
+
 end
